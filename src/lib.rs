21ceb6768c8ea452092e968307923656cb8af91a
Y. T. Chung <zonyitoo@gmail.com>
2015-08-09 22:19:14 +0800

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(unused_features)]
+#![feature(asm, libc, rustc_private, page_size, core_simd, core, alloc, rt)]
+
+#[macro_use]
+extern crate log;
+extern crate libc;
+extern crate mmap;
+
+pub use context::Context;
+pub use stack::Stack;
+
+pub mod context;
+pub mod stack;
+mod sys;
+mod thunk;
